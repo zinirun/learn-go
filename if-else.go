476b1e4895fcfa385602045f1bf5a2558898bbee
zinirun <heojj97@naver.com>
2021-01-18 14:24:01 +0900

    Delete files

--- if-else.go
+++
@@ -1,7 +0,0 @@
-// If Else, Variable Expression
-func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 { // ;로 변수 선언 후 검사할 수 있음 (조건에만 사용하기위해 변수 선언할 때)
-		return false
-	}
-	return true
-}
