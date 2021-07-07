13e9e61fb2a5f5a397593031451245a18bf6f7a7
Jeon Jin Heo <60263249+zinirun@users.noreply.github.com>
2021-07-07 12:18:33 +0900

    modify: http rest

--- http-rest/app_test.go
+++ http-rest/app_test.go
@@ -1,9 +1,11 @@
 package httprest
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,4 +23,31 @@ func TestGetUserInfo(t *testing.T) {
 
 	data, _ := ioutil.ReadAll(resp.Body)
 	assert.Contains(string(data), "User id: 89")
+}
+
+func TestCreateUserInfo(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(RestHandler())
+	defer ts.Close()
+
+	resp, err := http.Post(ts.URL+"/users", "application/json",
+		strings.NewReader(`
+	{
+		"firstname": "zini",
+		"lastname": "heo",
+		"email": "zini@willog.io"
+	}`))
+
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+
+	user := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user)
+
+	assert.NoError(err)
+	assert.NotEqual(0, user.ID)
+
+	id := user.ID
+	resp, err = http.Get(ts.URL)
 }
