bf22f91ef66f35c60cd49cd691e731dcaee5b74b
Jeon Jin Heo <60263249+zinirun@users.noreply.github.com>
2021-07-11 14:16:37 +0900

    modify: add handlers

--- http-rest/app.go
+++ http-rest/app.go
@@ -63,6 +63,43 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, string(data))
 }
 
+func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	if _, ok := userMap[id]; !ok {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "No User Id: ", id)
+		return
+	}
+
+	delete(userMap, id)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Deleted User Id: ", id)
+}
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+	if len(userMap) == 0 {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "No Users")
+		return
+	}
+	users := []*User{}
+	for _, user := range userMap {
+		users = append(users, user)
+	}
+
+	data, _ := json.Marshal(users)
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(data))
+}
+
 func RestHandler() http.Handler {
 	userMap = make(map[int]*User)
 	lastID = 0
@@ -70,8 +107,10 @@ func RestHandler() http.Handler {
 	mux := mux.NewRouter()
 
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/users", usersHandler).Methods("GET")
 	mux.HandleFunc("/users/{id:[0-9]+}", getUserInfoHandler).Methods("GET")
 	mux.HandleFunc("/users", createUserHandler).Methods("POST")
+	mux.HandleFunc("/users/{id:[0-9]+}", deleteUserHandler).Methods("DELETE")
 
 	http.ListenAndServe(":8888", mux)
 	return mux
